25632a0d81c73fc8f930e1925908f6f6a937f12d
unknown <secours@a-013838.upsay.universite-paris-saclay.fr>
2026-04-08 16:43:42 +0200

    v45

--- v3/lights.vhd
+++ v3/lights.vhd
@@ -4,13 +4,10 @@ USE ieee.numeric_std.all;
 
 ENTITY lights IS
     PORT (
-        SW        : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);  -- 4 switches
-        KEY       : IN  STD_LOGIC_VECTOR(0 DOWNTO 0);  -- 1 bouton reset
+        SW        : IN  STD_LOGIC_VECTOR(3 DOWNTO 0);
+        KEY       : IN  STD_LOGIC_VECTOR(0 DOWNTO 0);
         CLOCK_50  : IN  STD_LOGIC;
-
-        LED       : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);  -- 8 LEDs
-
-        -- ADC
+        LED       : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
         LTC_ADC_CONVST : OUT std_logic;
         LTC_ADC_SCK    : OUT std_logic;
         LTC_ADC_SDI    : OUT std_logic;
@@ -25,6 +22,12 @@ ARCHITECTURE Structure OF lights IS
     ------------------------------------------------
     signal clk_40M : std_logic;
     signal clk_2k  : std_logic;
+
+    ------------------------------------------------
+    -- détecteur flanc montant clk_2k
+    ------------------------------------------------
+    signal clk_2k_prev   : std_logic := '0';
+    signal capture_pulse : std_logic;
 
     ------------------------------------------------
     -- signaux capteurs
@@ -46,38 +49,50 @@ BEGIN
     PLL_inst : entity work.pll_2freqs
         port map(
             inclk0 => CLOCK_50,
-            c0    => clk_40M,
-            c1    => clk_2k
+            c0     => clk_40M,
+            c1     => clk_2k
         );
 
     ------------------------------------------------
-    -- sélection opération (switch)
+    -- détecteur flanc montant sur clk_2k
+    -- génère une impulsion d'1 cycle à 40 MHz
     ------------------------------------------------
-    op_sel <= SW(1 downto 0);  -- uniquement les 2 premiers switches
+    process(clk_40M, KEY)
+    begin
+        if KEY(0) = '0' then
+            clk_2k_prev <= '0';
+        elsif rising_edge(clk_40M) then
+            clk_2k_prev <= clk_2k;
+        end if;
+    end process;
+
+    capture_pulse <= '1' when (clk_2k = '1' and clk_2k_prev = '0') else '0';
+
+    ------------------------------------------------
+    -- sélection opération (switches)
+    ------------------------------------------------
+    op_sel <= SW(1 downto 0);
 
     ------------------------------------------------
     -- acquisition ADC
     ------------------------------------------------
     capteurs_inst : entity work.capteurs_sol
         port map(
-            clk         => clk_40M,
-            reset_n     => KEY(0),
-
-            data_capture => clk_2k,  -- acquisition périodique
+            clk          => clk_40M,
+            reset_n      => KEY(0),
+            data_capture => capture_pulse,   -- impulsion courte
             data_readyr  => data_ready,
-
-            data0r => data0,
-            data1r => data1,
-            data2r => open,
-            data3r => open,
-            data4r => open,
-            data5r => open,
-            data6r => open,
-
-            ADC_CONVSTr => LTC_ADC_CONVST,
-            ADC_SCK     => LTC_ADC_SCK,
-            ADC_SDIr    => LTC_ADC_SDI,
-            ADC_SDO     => LTC_ADC_SDO
+            data0r       => data0,
+            data1r       => data1,
+            data2r       => open,
+            data3r       => open,
+            data4r       => open,
+            data5r       => open,
+            data6r       => open,
+            ADC_CONVSTr  => LTC_ADC_CONVST,
+            ADC_SCK      => LTC_ADC_SCK,
+            ADC_SDIr     => LTC_ADC_SDI,
+            ADC_SDO      => LTC_ADC_SDO
         );
 
     ------------------------------------------------
@@ -85,20 +100,17 @@ BEGIN
     ------------------------------------------------
     calc_inst : entity work.calculateur
         port map(
-            clk      => CLOCK_50,
-            reset_n  => KEY(0),
-
-            A        => data0,
-            B        => data1,
-
-            op_sel   => op_sel,
-
-            result   => result_sig
+            clk     => CLOCK_50,
+            reset_n => KEY(0),
+            A       => data0,
+            B       => data1,
+            op_sel  => op_sel,
+            result  => result_sig
         );
 
     ------------------------------------------------
-    -- affichage résultat
+    -- affichage résultat (uniquement si donnée valide)
     ------------------------------------------------
-    LED <= result_sig(7 downto 0);
+    LED <= result_sig(7 downto 0) when data_ready = '1' else (others => '0');
 
 END Structure;
